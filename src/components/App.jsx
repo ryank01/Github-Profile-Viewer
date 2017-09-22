1d40d6484c1d3135980eeeebf8bd12a751075728
ryan <ryankawall01@gmail.com>
2017-09-22 01:29:14 -0400

    Github Profile viewer
    Added some styling to profile page. Created Profile component
    Repo component and RepoList component. Getting all the data in App
    and passing all the states to Profile component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import Profile from './github/Profile.jsx';
 class App extends Component{
   constructor(props){
     super(props);
@@ -11,10 +11,51 @@ class App extends Component{
       perPage: 5
     }
   }
+
+  // Get user data from github
+  getUserData(){
+    $.ajax({
+      url: 'https://api.github.com/users/'+ this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({userData: data});
+        console.log(data);
+      }.bind(this),
+      error: function(xhr, status, err){
+        this.setState({username: null});
+        alert(err);
+      }.bind(this)
+
+    });
+  }
+
+  // Get user repos from github
+  getUserRepos(){
+    $.ajax({
+      url: 'https://api.github.com/users/'+ this.state.username+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({userRepos: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        this.setState({username: null});
+        alert(err);
+      }.bind(this)
+
+    });
+  }
+
+  componentDidMount(){
+    this.getUserData();
+    this.getUserRepos();
+  }
+
   render(){
     return(
       <div>
-        {this.props.clientId}
+        <Profile {...this.state} />
       </div>
     )
   }
